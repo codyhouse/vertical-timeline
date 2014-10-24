7ac25693d186e8aa8455b895467512aade4dfe60
tekla_to <tekla_to@o2.pl>
2014-10-24 15:20:11 +0200

    separate styles for the demo page

---
+++ config.rb
@@ -0,0 +1,9 @@
+# Require any additional compass plugins here.
+add_import_path "bourbon"
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "img"
+javascripts_dir = "js"
