0c13600bd92052b5f4752b848ac987f74fc4dd70
Jasper Chang <jasperc8@gmail.com>
2015-03-24 17:50:10 +0800

    add fadeout feature when scrollup the viewport

--- js/main.js
+++ js/main.js
@@ -9,10 +9,16 @@ jQuery(document).ready(function($){
 	});
 
 	//on scolling, show/animate timeline blocks when enter the viewport
+	//fadeout/animate timeline when scrollup the viewport
 	$(window).on('scroll', function(){
 		$timeline_block.each(function(){
-			if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) {
-				$(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+			if( $(this).offset().top < $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-img').is('.is-hidden, .bounce-out') ) {
+                    $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden bounce-out').addClass('bounce-in');
+            }
+			if($(this).offset().top >= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-img').is('.bounce-in')) {
+				$(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('bounce-in').addClass('bounce-out');   
+			} else if($(this).offset().top >= $(window).scrollTop()+$(window).height()*0.80 && $(this).find('.cd-timeline-img').is('.bounce-out')) {
+				$(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('bounce-out').addClass('is-hidden');   
 			}
 		});
 	});
